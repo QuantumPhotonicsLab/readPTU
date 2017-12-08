b5f5370c815e935ed0b73013023dc1babd05d7bf
Raphael Proux <raphaelproux@gmail.com>
2017-12-08 16:34:05 +0000

    removed all other files, kept only readPTU -RProux

--- config.py
+++
@@ -1,9 +0,0 @@
-
-config = {}
-
-config['home_folder'] = 'D:/Research/Bonato-lab/'
-config['data_folder'] = 'D:/Research/WorkData'
-
-config['plot_settings'] = {
-	'axis_fontsize':		18,
-}
--- setup.py
+++ setup.py
@@ -1,57 +1,75 @@
-
-import sys
 import os
-import numpy as np
-import h5py
-import socket
+from setuptools import setup
+from setuptools.command.install import install
+import subprocess
 
 
-#hostpc = os.getenv ('computername')
-hostpc = socket.gethostname()
+from cffi import FFI
+from os import remove
+import setuptools  # necessary magic import for windows -- don't think, just accept it!
 
-settings = {}
+# To add a new parsers just add the appropiate parser in parsers.c with the
+# name convention Parse____ where the gap corresponds to the record
+# type format name. Then add the same name to the list below and you are done!
 
-settings ['DaleMac.local'] = {
-    'description': 'Dale Mac',
-    'folder': "/Users/dalescerri/Documents/GitHub/Spins/",
-    }
-settings['BAY3-HP'] = {
-	'description': 'Bay3 pc in DB2.17',
-	'folder': "C:/Research/",
-	}
-settings['cristian-mint'] = {
-	'description': 'Cristian - old TU Delft laptop',
-	'folder': "/home/cristian/Work/bonato-lab/",
-	}
-settings['cristian-PC'] = {
-	'description': 'Cristian - Thinkpad',
-	'folder': "C:/Users/cristian/Research/bonato-lab/",	
-}	
-settings['HWPC0526-EPS'] = {
-	'description': 'Cristian - office pc',
-	'folder': "H:/Research/bonato-lab/",
-	}
+# You will also have to add the new parser in readPTU.py in the method
+# _select_record_library of PTUmeasurement.
 
-print 'Loaded settings for: ', settings[hostpc]['description']
+# Finally as it is now the records are assumed to be 32 bits long. Using
+# the same conidtional compiling strategy as below this could be easily
+# modifiable for other types of records.
 
-folder = settings[hostpc]['folder']
-sys.path.append (folder)
+def compile_library():
+    parsers = ['HHT2_HH1', 'HHT2_HH2', 'PHT2']
 
-sys.path.append (folder+ '/analysis_simulations/')
-os.chdir (folder+ '/analysis_simulations/')
+    with open('./readPTU/readTTTRRecords-for-import.c', 'r') as myfile:
+        c_code = myfile.read()
+        codes_dict = {k: c_code.replace("##parser##", k) for k in parsers}
 
-global root_folder 
-root_folder = folder
+    prototypes = r"""
+        void timetrace(char filepath[], int end_of_header, uint64_t RecNum_start,
+                       uint64_t NumRecords, uint64_t time_bin_length,
+                       int time_trace[], uint64_t RecNum_trace[], int nb_of_bins,
+                       int n_threads);
+        void calculate_g2(char filepath[], int end_of_header,
+                          uint64_t *RecNum_start, uint64_t *RecNum_stop,
+                          int nb_of_ranges, uint64_t max_time, int histogram[],
+                          int nb_of_bins, int channel_start, int channel_stop,
+                          int buffer_size, int n_threads, int mode);
+        FILE *fdopen(int, const char *);   // from the C <stdio.h>
+        int fclose(FILE *);
+        int c_fseek(FILE *, long int);"""
 
-import matplotlib as mpl
-from matplotlib import pyplot as plt
-from matplotlib.colors import ListedColormap
-mpl.rc('xtick', labelsize=18) 
-mpl.rc('ytick', labelsize=18)
+    for code in codes_dict:
+        ffibuilder = FFI()
+        ffibuilder.cdef(prototypes)
+        ffibuilder.set_source("readPTU._readTTTRRecords_{}".format(code),
+                              codes_dict[code])
+                              # extra_compile_args=["-O3"])
+        print('\nCompiling version {}'.format(code))
+        ffibuilder.compile(verbose=False)
+        remove('./readPTU/_readTTTRRecords_{}.c'.format(code))
+        # remove('./readPTU/_readTTTRRecords_{}.o'.format(code))
 
-try:
-	_viridis_data = np.load (folder+'/viridis_cmp.npy')
-	viridis = ListedColormap(_viridis_data, name='viridis')
-	plt.register_cmap(name='viridis', cmap=viridis)
-except:
-	pass
+
+class Build(install):
+    """Custom handler for the 'install' command."""
+    def run(self):
+        # install.__init__(self)
+        compile_library()
+        install.run(self)
+
+
+setup(name='readPTU',
+      version='0.1',
+      description='Read and Analyze PicoQuant file formats like ptu, pt2, pt3 ...',
+      url='http://github.com/storborg/funniest',
+      author='QPL Lab',
+      author_email='gb173@hw.ac.uk',
+      license='MIT',
+      packages=['readPTU'],
+      zip_safe=False,
+      setup_requires=["cffi>=1.11.2"],
+      install_requires=["cffi>=1.11.2"],
+      cmdclass={'install': Build},
+      package_data={'': ['*.so', '*.c', '*.o']})
